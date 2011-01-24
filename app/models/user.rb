562b67e29d530d39eaa7943549d198aa05ace67b
Jon-Paul Lussier Jr <jonpaul.lussier@gmail.com>
2011-01-24 12:44:20 -0500

    Users have microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,23 @@
 # == Schema Information
-# Schema version: 20101217150935
+# Schema version: 20110120204231
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
+  
+  has_many :microposts, :dependent => :destroy
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -40,6 +45,11 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt ) ? user : nil
+  end
+  
+  def feed
+    #This is preliminary--
+    Micropost.where("user_id = ?", id)
   end
   
   private
