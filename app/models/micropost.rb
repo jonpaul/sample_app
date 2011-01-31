bae1b90b8e9a8e8be154e9887fafa9df8249fe44
Jon-Paul Lussier Jr <jonpaul.lussier@gmail.com>
2011-01-31 15:17:27 -0500

    Typo

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -33,7 +33,7 @@ class Micropost < ActiveRecord::Base
     #Return an SQL condition for users followed by the given user.
     #We include the user's own ID as well--
     def self.followed_by(user)
-      followed_ids = %(SELECT followed_idFROM relationships
+      followed_ids = %(SELECT followed_id FROM relationships
                        WHERE follower_id = :user_id)
       where("user_id IN (#{followed_ids}) OR user_id = :user_id",
             { :user_id => user })
